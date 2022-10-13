fa3f9268ba2ffc8792ee5b3d064637ea5faf2c32
SachinGadge13 <gadges14@gmail.com>
2022-10-13 22:04:00 +0530

    Added:Refactor all the 3 to one Generic Method and Find Maximum

--- src/com/bridgelabz/Generics/Main.java
+++ src/com/bridgelabz/Generics/Main.java
@@ -1,9 +1,14 @@
 package com.bridgelabz.Generics;
 
 public class Main {
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Check maximum of three variables");
-		String x = "Kiwi", y = "Berries", z = "banana";
+		System.out.println("Welcome to check Maximum of three variables");
+		String x = "Sachin", y = "Mohit", z = "Akshay";
+		Integer a = 30, b = 15, c = 55;
+		Float p = 6.23f, q = 3.2f, r = 0.2f;
 		TestMaximum.testMaximum(x, y, z);
+		TestMaximum.testMaximum(a, b, c);
+		TestMaximum.testMaximum(p, q, r);
 	}
 }
--- src/com/bridgelabz/Generics/TestMaximum.java
+++ src/com/bridgelabz/Generics/TestMaximum.java
@@ -1,18 +1,17 @@
 package com.bridgelabz.Generics;
 
-public class TestMaximum {
-	String x, y, z;
+public class TestMaximum<T extends Comparable<T>> {
+	T x, y, z;
 
-	// constructor
-	public TestMaximum(String x, String y, String z) {
+//constructor test maximum
+	public TestMaximum(T x, T y, T z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
 
-	// checking max of three integers
-	public static String testMaximum(String x, String y, String z) {
-		String max = x;// initialising x as greater
+	public static <T extends Comparable<T>> T testMaximum(T x, T y, T z) {
+		T max = x;// Initializing x as greater
 		if (y.compareTo(max) > 0)
 			max = y;// as of now y is greater
 
@@ -23,7 +22,7 @@ public class TestMaximum {
 
 	}
 
-	public static void printMax(String x, String y, String z, String max) {
-		System.out.printf("The maximum from string of %s , %s and %s is %s\n", x, y, z, max);
+	public static <T> void printMax(T x, T y, T z, T max) {
+		System.out.printf("the Max of following variables %s , %s and %s is %s\n", x, y, z, max);
 	}
 }
