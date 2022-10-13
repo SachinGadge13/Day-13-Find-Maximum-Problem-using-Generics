7791f4f86349489f25561059de2c9c864235c95f
SachinGadge13 <gadges14@gmail.com>
2022-10-13 21:50:55 +0530

    Added:program to find Maximum of Three Strings

--- src/com/bridgelabz/Generics/Main.java
+++ src/com/bridgelabz/Generics/Main.java
@@ -3,7 +3,7 @@ package com.bridgelabz.Generics;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Check maximum of three variables");
-		Float x = 5.5f, y = 6.5f, z = 3.65f;
+		String x = "Kiwi", y = "Berries", z = "banana";
 		TestMaximum.testMaximum(x, y, z);
 	}
 }
--- src/com/bridgelabz/Generics/TestMaximum.java
+++ src/com/bridgelabz/Generics/TestMaximum.java
@@ -1,29 +1,29 @@
 package com.bridgelabz.Generics;
 
 public class TestMaximum {
-	int x, y, z;
+	String x, y, z;
 
-//constructor testmax
-	public TestMaximum(int x, int y, int z) {
+	// constructor
+	public TestMaximum(String x, String y, String z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
 
-//checking max of three floats values x,y,z
-	public static Float testMaximum(Float x, Float y, Float z) {
-		Float max = x;// Initializing x as greater value
+	// checking max of three integers
+	public static String testMaximum(String x, String y, String z) {
+		String max = x;// initialising x as greater
 		if (y.compareTo(max) > 0)
-			max = y;// as of now y is greater in value
+			max = y;// as of now y is greater
 
 		if (z.compareTo(max) > 0)
-			max = z;// now z is greater in value
+			max = z;// now z is greater
 		printMax(x, y, z, max);
 		return max;
 
 	}
 
-	public static void printMax(Float x, Float y, Float z, Float max) {
-		System.out.printf("The maximum of Given Float values  %s , %s and %s is %s\n", x, y, z, max);
+	public static void printMax(String x, String y, String z, String max) {
+		System.out.printf("The maximum from string of %s , %s and %s is %s\n", x, y, z, max);
 	}
 }
